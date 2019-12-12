2df463f1803e4849d6cf12a8eb0c8271e95843e7
DeveloperZG <mrzhugang1993@163.com>
2019-12-12 16:51:18 +0800

    使用类信息创建对象。

--- app/src/main/java/com/thoughtworks/themoments/patterndesign/factor/ConcreteFactory.java
+++ app/src/main/java/com/thoughtworks/themoments/patterndesign/factor/ConcreteFactory.java
@@ -5,7 +5,13 @@ package com.thoughtworks.themoments.patterndesign.factor;
  */
 public class ConcreteFactory extends Factory {
     @Override
-    public Product createProduct() {
-        return new ConcreteProductA();
+    public <T extends Product> T createProduct(Class<T> clz) {
+        Product product = null;
+        try {
+            product = (Product) Class.forName(clz.getName()).newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return (T) product;
     }
 }
--- app/src/main/java/com/thoughtworks/themoments/patterndesign/factor/Factory.java
+++ app/src/main/java/com/thoughtworks/themoments/patterndesign/factor/Factory.java
@@ -4,5 +4,5 @@ package com.thoughtworks.themoments.patterndesign.factor;
  * Created by Zhu on 2019-12-12
  */
 public abstract class Factory {
-    public abstract Product createProduct();
+    public abstract <T extends Product> T createProduct(Class<T> clz);
 }
