18b44581fb064d09596e33f9ca7c8e9140e46591
DeveloperZG <mrzhugang1993@163.com>
2019-12-20 14:50:27 +0800

    Factory.

---
+++ app/src/main/java/com/thoughtworks/themoments/patterndesign/headfirst/factory/NYPizzaStore.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.themoments.patterndesign.headfirst.factory;
+
+/**
+ * Created by Zhu on 2019-12-19
+ */
+public class NYPizzaStore extends PizzaStore {
+    @Override
+    protected Pizza createPizza(String type) {
+        return null;
+    }
+}
